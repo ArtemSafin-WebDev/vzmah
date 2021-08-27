663b320b570f3579d2328b860762f16762a1c474
Artem Safin <artemsafin.webdev@gmail.com>
2021-08-27 17:39:03 +0300

    Правки

--- src/js/main.js
+++ src/js/main.js
@@ -5,15 +5,16 @@ import setScrollbarWidth from './setScrollbarWidth';
 import validation from './validation';
 import customSelects from './customSelects';
 import masks from './masks';
-import fileUpload from './fileUpload';
+
 import anchorLinks from './anchorLinks';
 import mediaPlayer from './mediaPlayer';
-import datepicker from './datepicker';
+
 import accordions from './accordions';
 import modals from './modals';
 
 import barba from '@barba/core';
 import barbaCss from '@barba/css';
+import fancybox from './fancybox';
 
 barba.use(barbaCss);
 
@@ -21,15 +22,15 @@ document.addEventListener('DOMContentLoaded', function() {
     polyfills();
     detectTouch();
     setScrollbarWidth();
+    fancybox();
     // validation();
     // customSelects();
     // masks();
-    // fileUpload();
     // anchorLinks();
     // accordions();
     // mediaPlayer();
     // modals();
-    // datepicker();
+   
 
     barba.init({
         transitions: [
